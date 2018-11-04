859da3d96e1733abb751eca1d783bcdfb91483a8
Konstantin Zelmanovich <konstantin@siftsec.com>
2018-11-04 02:04:39 -0500

    Add Two Sum

--- 1-Two-Sum.py
+++ 1-Two-Sum.py
@@ -1,7 +1,13 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+def twoSum(nums, target):
+    compliments = {}
+    for i in range(len(nums)):
+        compliment = target - nums[i]
+        if compliment in compliments:
+            return i, compliments[compliment]
+        else:
+            compliments[nums[i]] = i
+
+
+nums = [2, 7, 11, 15]
+target = 9
+print twoSum(nums, target)
