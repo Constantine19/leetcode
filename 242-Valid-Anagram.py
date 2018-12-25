e9b0a81e3c8c661eb73b7508d9c9876beb293071
Konstantin Zelmanovich <konstantin@siftsec.com>
2018-12-25 04:21:21 -0500

    more

--- 242-Valid-Anagram.py
+++ 242-Valid-Anagram.py
@@ -1,9 +1,14 @@
 def isAnagram(s, t):
-    for i in s:
-        if s.count(i) != t.count(i):
-            return False
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    for i in alphabet:
+        if s.count(i) == t.count(i):
+            continue
         else:
-            return True
+            return False
+
+    return True
 
 
 s = "anagram"
--- 500-Keyboard-Row.py
+++ 500-Keyboard-Row.py
@@ -1,23 +1,21 @@
 def findWords(words):
-    first_row = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p']
-    second_row = ['a', 's', 'd',  'f', 'g',  'h', 'j', 'k', 'l']
-    third_row = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+    first_row = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'}
+    second_row = {'a', 's', 'd',  'f', 'g',  'h', 'j', 'k', 'l'}
+    third_row = {'z', 'x', 'c', 'v', 'b', 'n', 'm'}
 
-    print first_row
-    print second_row
-    print third_row
+    keyboard = [first_row, second_row, third_row]
 
-    counts = {}
+    valid_words = []
+
     for word in words:
-        for letter in word:
-            if letter not in counts:
-                counts[letter] = 1
-        print i
+        word_set = set(word.lower())
+        for row in keyboard:
+            if word_set.issubset(row):
+                valid_words.append(word)
+                break
 
-    print words
-
-
+    return valid_words
 
 
 words = ["Hello", "Alaska", "Dad", "Peace"]
-findWords(words)
+print findWords(words)
